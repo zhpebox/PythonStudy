ac70d6394db4efe4626d1dd292c60db704bccf89
peng <zhpebox@163.com>
2017-12-05 14:02:49 +0800

    update file

--- tensorFlow/input_data.py
+++ tensorFlow/input_data.py
@@ -28,8 +28,5 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
-# import tensorflow.examples.tutorials.mnist.input_data
-# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
 
 
